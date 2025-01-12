4339a4e839b02525ebf88115c74a7e03a73b14b7
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2025-01-12 18:10:13 +0500

    Добавил favicon для значка веб-страницы

--- django/ulearn_vacs_project/urls.py
+++ django/ulearn_vacs_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='/static/frame/images/favicon.ico', permanent=True)),
     path('', include('general_page.urls')),
 ]
