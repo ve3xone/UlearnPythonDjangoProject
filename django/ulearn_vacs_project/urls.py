9a41cf95fc1da4dc5a6f543171319fa7b8963059
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2025-01-14 10:36:12 +0500

    Добавил общую стату в сам проект (urls)

--- django/ulearn_vacs_project/urls.py
+++ django/ulearn_vacs_project/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('favicon.ico', RedirectView.as_view(url='/static/frame/images/favicon.ico', permanent=True)),
     path('', include('general_page.urls')),
+    path('general_stats', include('general_stats_page.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
