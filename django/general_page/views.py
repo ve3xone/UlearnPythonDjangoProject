a4dba0c8a3a28a028cf290d973f4ae1520ec6258
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2025-01-14 10:34:04 +0500

    Заделка на будущее (AJAX)
    Прогрузка контекта без перезагрузки страницы

--- django/general_page/views.py
+++ django/general_page/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def general_page(request):
     return render(request, 'general.html')
+    # Проверка, AJAX-запрос это или нет
+    # if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+    #     # Возвращаем только контент для AJAX
+    #     return render(request, 'general_partial.html')
+    # else:
+    #     # Возвращаем полный шаблон
+    #     return render(request, 'general.html')
