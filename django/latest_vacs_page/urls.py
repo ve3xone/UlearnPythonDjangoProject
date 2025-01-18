2ba74d45bb0ac88d24053758d76e147a2e4aa5c5
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2025-01-19 03:06:15 +0500

    Поправил

--- django/latest_vacs_page/urls.py
+++ django/latest_vacs_page/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('/api/', fetch_vacancies, name='fetch_vacancies'),
+    path('/api', fetch_vacancies, name='fetch_vacancies'),
     path('', render_vacancies, name='render_vacancies'),
 ]
