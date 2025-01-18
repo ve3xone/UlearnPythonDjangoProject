f6a9adef43423e5b9f570988ebde0e75bdc04b0e
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2025-01-19 02:35:11 +0500

    Ошибку убрал

--- django/ulearn_vacs_project/settings.py
+++ django/ulearn_vacs_project/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = '/home/ve3xone/django/static' # For pythonanywhere (python manage.py collectstatic)
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
