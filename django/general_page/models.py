4e9316df79f306cb8b712716df40baa211cd46f6
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2025-01-19 15:17:35 +0500

    Сделал CRUD для главной страницы

--- django/general_page/models.py
+++ django/general_page/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class General(models.Model):
+    """Моделька главной страницы"""
+    vac_pic = models.ImageField(
+        blank=True,
+        null=True,
+        default='/General_JvmSpec7.png',
+        verbose_name='Картинка вакансии'
+    )
+    vac_text = models.TextField(
+        blank=True,
+        null=True,
+        default=settings.DEFAULT_VAC_TEXT,
+        verbose_name='Текст вакансии (HTML)'
+    )
+
+
+    class Meta:
+        verbose_name = 'Главная'
+        verbose_name_plural = verbose_name
--- django/general_page/views.py
+++ django/general_page/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import General
 
 
 def general_page(request):
     """Рендер главной страницы"""
-    return render(request, 'general_page.html')
+    general_obj = General.objects.first()
+
+    # Доступ к default
+    default_vac_pic= General._meta.get_field('vac_pic').default
+    default_vac_text = General._meta.get_field('vac_text').default
+
+    content = {
+        'vac_pic': general_obj.vac_pic.url if general_obj and general_obj.vac_pic else '/media'+default_vac_pic,
+        'vac_text': general_obj.vac_text if general_obj else default_vac_text,
+    }
+    return render(request, 'general_page.html', content)
