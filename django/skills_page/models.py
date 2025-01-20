3799efb1ecb3dfdb144717c23ee62e4c08c07c0c
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2025-01-20 12:36:26 +0500

    Рефакторинг страницы навыков

--- django/skills_page/models.py
+++ django/skills_page/models.py
@@ -3,96 +3,97 @@ from django.db import models
 
 class SkillsStatistics(models.Model):
     """"Модель страницы навыки"""
-    top20_skills_java_2015_graph = models.ImageField(
-        blank=False, 
+    graph_top20_sks_java_2015 = models.ImageField(
+        blank=False,
         verbose_name='График навыков за 2015 год для Java-программиста'
     )
-    top20_skills_java_2015_data = models.TextField(
-        blank=False, 
+    data_table_top20_sks_java_2015 = models.TextField(
+        blank=False,
         verbose_name='Таблица навыков за 2015 год для Java-программиста'
     )
 
-    top20_skills_java_2016_graph = models.ImageField(
-        blank=False, 
+    graph_top20_sks_java_2016 = models.ImageField(
+        blank=False,
         verbose_name='График навыков за 2016 год для Java-программиста'
     )
-    top20_skills_java_2016_data = models.TextField(
-        blank=False, 
+    data_table_top20_sks_java_2016 = models.TextField(
+        blank=False,
         verbose_name='Таблица навыков за 2016 год для Java-программиста'
     )
 
-    top20_skills_java_2017_graph = models.ImageField(
-        blank=False, 
+    graph_top20_sks_java_2017 = models.ImageField(
+        blank=False,
         verbose_name='График навыков за 2017 год для Java-программиста'
     )
-    top20_skills_java_2017_data = models.TextField(
-        blank=False, 
+    data_table_top20_sks_java_2017 = models.TextField(
+        blank=False,
         verbose_name='Таблица навыков за 2017 год для Java-программиста'
     )
 
-    top20_skills_java_2018_graph = models.ImageField(
-        blank=False, 
+    graph_top20_sks_java_2018 = models.ImageField(
+        blank=False,
         verbose_name='График навыков за 2018 год для Java-программиста'
     )
-    top20_skills_java_2018_data = models.TextField(
-        blank=False, 
+    data_table_top20_sks_java_2018 = models.TextField(
+        blank=False,
         verbose_name='Таблица навыков за 2018 год для Java-программиста'
     )
 
-    top20_skills_java_2019_graph = models.ImageField(
-        blank=False, 
+    graph_top20_sks_java_2019 = models.ImageField(
+        blank=False,
         verbose_name='График навыков за 2019 год для Java-программиста'
     )
-    top20_skills_java_2019_data = models.TextField(
-        blank=False, 
+    data_table_top20_sks_java_2019 = models.TextField(
+        blank=False,
         verbose_name='Таблица навыков за 2019 год для Java-программиста'
     )
 
-    top20_skills_java_2020_graph = models.ImageField(
-        blank=False, 
+    graph_top20_sks_java_2020 = models.ImageField(
+        blank=False,
         verbose_name='График навыков за 2020 год для Java-программиста'
     )
-    top20_skills_java_2020_data = models.TextField(
-        blank=False, 
+    data_table_top20_sks_java_2020 = models.TextField(
+        blank=False,
         verbose_name='Таблица навыков за 2020 год для Java-программиста'
     )
 
-    top20_skills_java_2021_graph = models.ImageField(
-        blank=False, 
+    graph_top20_sks_java_2021 = models.ImageField(
+        blank=False,
         verbose_name='График навыков за 2021 год для Java-программиста'
     )
-    top20_skills_java_2021_data = models.TextField(
-        blank=False, 
+    data_table_top20_sks_java_2021 = models.TextField(
+        blank=False,
         verbose_name='Таблица навыков за 2021 год для Java-программиста'
     )
 
-    top20_skills_java_2022_graph = models.ImageField(
-        blank=False, 
+    graph_top20_sks_java_2022 = models.ImageField(
+        blank=False,
         verbose_name='График навыков за 2022 год для Java-программиста'
     )
-    top20_skills_java_2022_data = models.TextField(
-        blank=False, 
+    data_table_top20_sks_java_2022 = models.TextField(
+        blank=False,
         verbose_name='Таблица навыков за 2022 год для Java-программиста'
     )
 
-    top20_skills_java_2023_graph = models.ImageField(
-        blank=False, 
+    graph_top20_sks_java_2023 = models.ImageField(
+        blank=False,
         verbose_name='График навыков за 2023 год для Java-программиста'
     )
-    top20_skills_java_2023_data = models.TextField(
-        blank=False, 
+    data_table_top20_sks_java_2023 = models.TextField(
+        blank=False,
         verbose_name='Таблица навыков за 2023 год для Java-программиста'
     )
 
-    top20_skills_java_2024_graph = models.ImageField(
-        blank=False, 
+    graph_top20_sks_java_2024 = models.ImageField(
+        blank=False,
         verbose_name='График навыков за 2024 год для Java-программиста'
     )
-    top20_skills_java_2024_data = models.TextField(
-        blank=False, 
+    data_table_top20_sks_java_2024 = models.TextField(
+        blank=False,
         verbose_name='Таблица навыков за 2024 год для Java-программиста'
     )
-    
+
+
     class Meta:
         verbose_name = 'Навыки'
         verbose_name_plural = verbose_name
--- django/skills_page/views.py
+++ django/skills_page/views.py
@@ -7,26 +7,35 @@ def skills_page(request):
     skills_vac_stats = SkillsStatistics.objects.first()
 
     content = {
-        'top20_skills_java_2015_graph': skills_vac_stats.top20_skills_java_2015_graph.url,
-        'top20_skills_java_2015_data': skills_vac_stats.top20_skills_java_2015_data,
-        'top20_skills_java_2016_graph': skills_vac_stats.top20_skills_java_2016_graph.url,
-        'top20_skills_java_2016_data': skills_vac_stats.top20_skills_java_2016_data,
-        'top20_skills_java_2017_graph': skills_vac_stats.top20_skills_java_2017_graph.url,
-        'top20_skills_java_2017_data': skills_vac_stats.top20_skills_java_2017_data,
-        'top20_skills_java_2018_graph': skills_vac_stats.top20_skills_java_2018_graph.url,
-        'top20_skills_java_2018_data': skills_vac_stats.top20_skills_java_2018_data,
-        'top20_skills_java_2019_graph': skills_vac_stats.top20_skills_java_2019_graph.url,
-        'top20_skills_java_2019_data': skills_vac_stats.top20_skills_java_2019_data,
-        'top20_skills_java_2020_graph': skills_vac_stats.top20_skills_java_2020_graph.url,
-        'top20_skills_java_2020_data': skills_vac_stats.top20_skills_java_2020_data,
-        'top20_skills_java_2021_graph': skills_vac_stats.top20_skills_java_2021_graph.url,
-        'top20_skills_java_2021_data': skills_vac_stats.top20_skills_java_2021_data,
-        'top20_skills_java_2022_graph': skills_vac_stats.top20_skills_java_2022_graph.url,
-        'top20_skills_java_2022_data': skills_vac_stats.top20_skills_java_2022_data,
-        'top20_skills_java_2023_graph': skills_vac_stats.top20_skills_java_2023_graph.url,
-        'top20_skills_java_2023_data': skills_vac_stats.top20_skills_java_2023_data,
-        'top20_skills_java_2024_graph': skills_vac_stats.top20_skills_java_2024_graph.url,
-        'top20_skills_java_2024_data': skills_vac_stats.top20_skills_java_2024_data,
+        'graph_top20_sks_java_2015_url': skills_vac_stats.graph_top20_sks_java_2015.url,
+        'data_table_top20_sks_java_2015': skills_vac_stats.data_table_top20_sks_java_2015,
+
+        'graph_top20_sks_java_2016_url': skills_vac_stats.graph_top20_sks_java_2016.url,
+        'data_table_top20_sks_java_2016': skills_vac_stats.data_table_top20_sks_java_2016,
+
+        'graph_top20_sks_java_2017_url': skills_vac_stats.graph_top20_sks_java_2017.url,
+        'data_table_top20_sks_java_2017': skills_vac_stats.data_table_top20_sks_java_2017,
+
+        'graph_top20_sks_java_2018_url': skills_vac_stats.graph_top20_sks_java_2018.url,
+        'data_table_top20_sks_java_2018': skills_vac_stats.data_table_top20_sks_java_2018,
+
+        'graph_top20_sks_java_2019_url': skills_vac_stats.graph_top20_sks_java_2019.url,
+        'data_table_top20_sks_java_2019': skills_vac_stats.data_table_top20_sks_java_2019,
+
+        'graph_top20_sks_java_2020_url': skills_vac_stats.graph_top20_sks_java_2020.url,
+        'data_table_top20_sks_java_2020': skills_vac_stats.data_table_top20_sks_java_2020,
+
+        'graph_top20_sks_java_2021_url': skills_vac_stats.graph_top20_sks_java_2021.url,
+        'data_table_top20_sks_java_2021': skills_vac_stats.data_table_top20_sks_java_2021,
+
+        'graph_top20_sks_java_2022_url': skills_vac_stats.graph_top20_sks_java_2022.url,
+        'data_table_top20_sks_java_2022': skills_vac_stats.data_table_top20_sks_java_2022,
+
+        'graph_top20_sks_java_2023_url': skills_vac_stats.graph_top20_sks_java_2023.url,
+        'data_table_top20_sks_java_2023': skills_vac_stats.data_table_top20_sks_java_2023,
+
+        'graph_top20_sks_java_2024_url': skills_vac_stats.graph_top20_sks_java_2024.url,
+        'data_table_top20_sks_java_2024': skills_vac_stats.data_table_top20_sks_java_2024,
     }
 
     return render(request, 'skills_page.html', content)
